487f3c8fe87c8497cad15bd9ac4ef0f0b91cdf7f
awanish.ranjan5@gmail.com <awanish.infarna@gmail.com>
2023-08-18 22:27:22 +0530

    pushing variables ami-datasources outputs

--- terraform-step2/c1-versions.tf
+++ terraform-step2/c1-versions.tf
@@ -2,17 +2,17 @@
 terraform {
   required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
-    aws = { # this is the local name of the provider, it can be anything, not necessarily aws. Just note that it is refered in the block below and should match. 
-      source = "hashicorp/aws" # here the registry name is not mentioned as by default it takes hashicorp registry, if any other registry is needed, mention registry
+    aws = {
+      source  = "hashicorp/aws"
       version = "~> 3.0"
     }
-  } 
-}  
-# Provider Block - the block lable here "aws" should be same as the name of the provider provided in the required_provider block
-provider "aws" {
-  region = "us-east-1"
+  }
 }
 
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+}
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
 $HOME/.aws/credentials
