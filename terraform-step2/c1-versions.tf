3b2d29921866edaea5ef8700795f67db8418a014
awanish.ranjan5@gmail.com <awanish.infarna@gmail.com>
2023-08-18 22:04:32 +0530

    creating an ec2 using the settings

--- terraform-step2/c1-versions.tf
+++ terraform-step2/c1-versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
     aws = { # this is the local name of the provider, it can be anything, not necessarily aws. Just note that it is refered in the block below and should match. 
-      source = "hashicorp/aws"
+      source = "hashicorp/aws" # here the registry name is not mentioned as by default it takes hashicorp registry, if any other registry is needed, mention registry
       version = "~> 3.0"
     }
   } 
