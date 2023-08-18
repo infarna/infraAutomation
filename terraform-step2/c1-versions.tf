8ae1f99baf5c165e290858a06b43e2701b0a5826
awanish.ranjan5@gmail.com <awanish.infarna@gmail.com>
2023-08-18 18:46:27 +0530

    terraform settings with commnets

--- terraform-step2/c1-versions.tf
+++ terraform-step2/c1-versions.tf
@@ -2,13 +2,13 @@
 terraform {
   required_version = ">= 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
-    aws = {
+    aws = { # this is the local name of the provider, it can be anything, not necessarily aws. Just note that it is refered in the block below and should match. 
       source = "hashicorp/aws"
       version = "~> 3.0"
     }
   } 
 }  
-# Provider Block
+# Provider Block - the block lable here "aws" should be same as the name of the provider provided in the required_provider block
 provider "aws" {
   region = "us-east-1"
 }
