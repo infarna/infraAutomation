723068356fd2d6c3e487752bc3188f20b7e4356c
awanish.ranjan5@gmail.com <awanish.infarna@gmail.com>
2023-08-21 10:55:03 +0530

    adding comment for data sources

--- terraform-step2/c4-ami-datasource.tf
+++ terraform-step2/c4-ami-datasource.tf
@@ -1,4 +1,12 @@
 # Get latest AMI ID for Amazon Linux2 OS
+/* this information is obtained from the documentation 
+by getting the example usage - Datasource aws_ami
+similar to any resource block, data block is there followed by datasource type and local name */
+
+/* the filters here contain two fileds, name and value
+name is the name of the data resource attribute and value is its value. */
+/* every data source has its own set of argutments and attributes and filters can be used based on that */
+
 data "aws_ami" "amzlinux2" {
   most_recent      = true
   owners           = ["amazon"]
